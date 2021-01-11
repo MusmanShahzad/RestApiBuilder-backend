856d396183d377ed25a2febc5298930148d5db13
Muhammad Usman Shahzad <usmanshahzad641@gmail.com>
2021-01-12 01:02:04 +0500

    databas ecredentails added

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ const defaultDir = 'userRestapi';
 const startServer = async () => {
     try {
         await mongoose.connect(
-            `mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=false`, {
+            `mongodb+srv://test:test@cluster0.00xdi.mongodb.net/RestapiBuilder?retryWrites=true&w=majority`, {
                 useUnifiedTopology: true,
                 useNewUrlParser: true,
                 useFindAndModify: false
